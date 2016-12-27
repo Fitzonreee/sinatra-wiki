92debfada29867e7937fded07eb43e2442a9a8b3
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-12-27 15:32:08 -0800

    Created new form for submitting new entires and post to a create route

--- wiki.rb
+++ wiki.rb
@@ -1,22 +1,5 @@
 require "sinatra"
 
-# routes
-get "/" do
-  @name = "Kevin"
-  erb :welcome
-end
-
-get "/kevin" do
-  "Yo Kev, what's goody?"
-end
-
-get "/:title" do
-  @title = params[:title]
-  @content = page_content(@title)
-  @title.sub!("-"," ")
-  erb :show
-end
-
 # get content of .txt in pages directory and convert to string
 def page_content(title)
   File.read("pages/#{title}.txt")
@@ -31,6 +14,27 @@ def save_content(title, content)
 end
 
 # run method and pass filename
-puts page_content("Kevin-Fitzhenry")
+# puts page_content("Kevin-Fitzhenry")
 # save arguements as .txt file
-save_content("Ryan-Fitzhenry", "Videograpther and also, the man.")
+# save_content("Ryan-Fitzhenry", "Videograpther and also, the man.")
+
+# routes
+get "/" do
+  @name = "Kevin"
+  erb :welcome
+end
+
+get "/new" do
+  erb :new
+end
+
+get "/:title" do
+  @title = params[:title]
+  @content = page_content(@title)
+  @title.sub!("-"," ")
+  erb :show
+end
+
+post "/create" do
+  
+end
