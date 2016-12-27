05c5e6e9659319eef40075db227e2295337e8806
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-12-27 14:20:50 -0800

    Converting static .txt to string and rendering in console

--- wiki.rb
+++ wiki.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 
+# routes
 get "/" do
-  "<h1>Welcome to our Wiki</h1>"
+  @name = "Kevin"
+  erb :welcome
 end
 
 get "/kevin" do
   "Yo Kev, what's goody?"
 end
+
+# get content of .txt in pages directory and convert to string
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return nil
+end
+
+# run method and pass filename
+puts page_content("Kevin_Fitzhenry")
